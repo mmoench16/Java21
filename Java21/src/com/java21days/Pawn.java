522fea942d3b3ff993a2e255f7e2694ce3cf9999
Martin Moench <martin.moench@roche.com>
2021-05-07 15:56:53 +0200

    Mars and ChessPieces

---
+++ Java21/src/com/java21days/Pawn.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java21days;
+
+/**
+ *
+ * @author moenchm1
+ */
+public class Pawn extends ChessPiece {
+    int forwardMovement = 1;
+    int sideMovement = 0;
+}
