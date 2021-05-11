7ca72c6bbeb5b6ba62cd99a2adc655124dfa2729
Martin Moench <martin.moench@roche.com>
2021-05-11 15:29:19 +0200

    Chapter 3, a start

---
+++ Java21/src/com/java21days/ch3/PointSetter.java
@@ -0,0 +1,21 @@
+package com.java21days.ch3;
+
+import java.awt.Point;
+
+class PointSetter {
+    public static void main(String[] args) {
+        Point location = new Point(4, 13);
+        
+        System.out.println("Starting location:");
+        System.out.println("X equals " + location.x);
+        System.out.println("X equals " + location.y);
+        
+        System.out.println("\nMoving to (7, 6)");
+        location.x = 7;
+        location.y = 6;
+        
+        System.out.println("\nEnding location:");
+        System.out.println("X equals " + location.x);
+        System.out.println("X equals " + location.y);
+    }
+}
